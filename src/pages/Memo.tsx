044a8077a095b4d149612316069dee4008e01311
yiinuma <y.iinuma211@gmail.com>
2022-04-10 15:34:39 +0900

    issues#9 Logout componet削除しMemoにlogout機能を実装

--- src/pages/Memo.tsx
+++ src/pages/Memo.tsx
@@ -1,25 +1,31 @@
 import { memo, VFC } from 'react';
 import { RecoilRoot } from 'recoil';
-import { LogoutButton } from '../components/button/LogoutButton';
 
+import { PrimaryButton } from '../components/button/PrimaryButton';
 import { InputForm } from '../components/InputForm';
 import { Layout } from '../components/Layout/Layout';
 import { MainLayout } from '../components/Layout/MainLayout';
 import { Modal } from '../components/Modal';
 import { Title } from '../components/Title';
 import { TodoList } from '../components/TodoList';
+import { useAuth } from '../hooks/useAuth';
 import '../style.css';
 
-export const Memo: VFC = memo(() => (
-  <RecoilRoot>
-    <Layout>
-      <LogoutButton />
-      <MainLayout>
-        <Title />
-        <InputForm />
-        <TodoList />
-        <Modal />
-      </MainLayout>
-    </Layout>
-  </RecoilRoot>
-));
+export const Memo: VFC = memo(() => {
+  const { logout } = useAuth();
+  return (
+    <RecoilRoot>
+      <Layout>
+        <div className="mr-8 mt-2 flex flex-row justify-end">
+          <PrimaryButton onClick={logout}>Logout</PrimaryButton>
+        </div>
+        <MainLayout>
+          <Title />
+          <InputForm />
+          <TodoList />
+          <Modal />
+        </MainLayout>
+      </Layout>
+    </RecoilRoot>
+  );
+});
